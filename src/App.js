c5b1a6a0d9b2faa855573a5643141d790b943a65
Kolten ✔️ <kolten.pilar@gmail.com>
2023-07-20 10:52:57 -0400

    created counter using state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import Arrays from "./Arrays";
 import Objects from "./Objects";
+import { useState } from "react";
 
 function App() {
+
+  const [counter, setCounter] = useState(0);
+
+  const handleIncrement = () => {
+    setCounter(counter + 1)
+  }
+  
+  const handleDecrement = () => {
+    setCounter(counter - 1)
+  }
+
   return (
     <div className="App">
       <h1>React Drills</h1>
       {/* <Arrays /> */}
-      <Objects />
+      {/* <Objects /> */}
+      <h1>{counter}</h1>
+      <button onClick={handleIncrement}>+</button>
+      <button onClick={handleDecrement}>-</button>
+      <br />
+      <button onClick={()=> {setCounter(0)}}>reset</button>
     </div>
   );
 }
